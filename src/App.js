bec238609ee813e87df14743d7e623bcdebbb559
Cameron <cameronkwalden@gmail.com>
2021-12-20 16:07:53 -0800

    brings in LoginButton, isLoggedIn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React, { useState } from 'react';
 import './App.css';
+import LoginButton from './components/login/LoginButton';
 
 export default function App() {
-  return <></>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <>
+    <LoginButton
+    isLoggedIn={isLoggedIn}
+    setIsLoggedIn={setIsLoggedIn}/>
+    </>
+  )
 }
