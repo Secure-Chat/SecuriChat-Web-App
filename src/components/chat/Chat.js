d92a613b32e6840f7664e136a694738522fab2c4
AntoineCharette <antoineocharette@gmail.com>
2021-12-22 12:53:18 -0500

    adds more logic to chat and messages component. Starts work on contacts js

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,13 +1,25 @@
 //imports
-import React from 'react'
+import React from 'react';
 
 //components
-import Message from '../message/Message'
+import Message from '../message/Message';
 
 export default function Chat(props) {
+  const handleMessage = (e) => {
+    // Insert message adding software here... can be code from reducer
+  };
+
   return (
     <>
-      {friend.messages.map(message => <Message message={message}/>)}
+      <div>
+        {props.messages.map((message) => (
+          <Message message={message} />
+        ))}
+      </div>
+      <form onSubmit={handleMessage}>
+        <textarea onChange={props.setMessage}></textarea>
+        <button type="submit">Send</button>
+      </form>
     </>
-  )
+  );
 }
--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,3 +1,34 @@
-export default function Contacts() {
-  return <></>;
-}
+//imports
+import React from 'react';
+import { connect } from 'react-redux';
+
+//components
+import Chat from '../chat/Chat';
+
+const mapStateToProps = (state) => {
+  return {
+    contactlist: state.contactlist,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(function Contacts(props) {
+  const handleClick = (e) => {};
+
+  return (
+    <>
+      {props.contactlist.map((contact) => {
+        return (
+          <div value={contact.messages} onClick={handleClick}>
+            <h1>{contact.username}</h1>
+            <p>{contact.messages[contact.messages.length - 1]}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+});
