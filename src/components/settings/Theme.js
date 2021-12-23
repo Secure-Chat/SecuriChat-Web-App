3617abebcd923d648d47c5379e2b1cf6c15d7496
Cameron <cameronkwalden@gmail.com>
2021-12-22 17:07:09 -0800

    refactors toggle functionality

--- src/components/settings/Theme.js
+++ src/components/settings/Theme.js
@@ -1,21 +1,32 @@
 import React, {useState} from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { createTheme } from '@mui/material/styles';
-import { Switch } from '@mui/material';
+import { CssBaseline, Paper, Switch, Typography } from '@mui/material';
+// import Brightness4Icon from '@mui/icons-material/Brightness4';
+// import Brightness5Icon from '@mui/icons-material/Brightness5';
 
 const Theme = props => {
-
+  
   const [darkMode, setDarkMode] = useState(false);
 
   const changeTheme = createTheme({
     palette: {
-      mode: darkMode ? 'dark' : 'light',
+      type: darkMode ? 'dark' : 'light'
     },
   })
 
   return (
     <ThemeProvider theme={changeTheme}>
+      <CssBaseline/>
+      <Paper>
       <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)}/>
+      {/* <IconButton color="inherit" onClick={() => setDarkMode(!darkMode)}>
+        {darkMode ? <Brightness4Icon/> :  <Brightness5Icon/>}
+      </IconButton> */}
+      <Typography variant="h1" >
+        {darkMode ? <p class="dark">Dark</p> : <p class="light">Light</p>}
+      </Typography>
+      </Paper>
     </ThemeProvider>
   )
 }
