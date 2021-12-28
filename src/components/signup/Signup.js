cff81d362e9606b10dc7d22efdac097ca9ef57b3
AntoineCharette <antoineocharette@gmail.com>
2021-12-28 12:16:53 -0500

    changes let to const on sign up// prettier formatted

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -13,22 +13,22 @@ const Signup = (props) => {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [redirect, setRedirect] = useState(false);
-  
+
   const handleUsername = (e) => {
     setUsername(e.target.value);
-  }
+  };
 
   const handleEmail = (e) => {
     setEmail(e.target.value);
-  }
+  };
 
-  const handlePassword = e => {
+  const handlePassword = (e) => {
     setPassword(e.target.value);
-  }
+  };
 
-  const handleConfirmPassword = e => {
+  const handleConfirmPassword = (e) => {
     setConfirmPassword(e.target.value);
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -36,20 +36,22 @@ const Signup = (props) => {
     // Check to make sure passwords match
     if (password === confirmPassword && password.length >= 8) {
       const payload = { username, email, password };
-      let url = `${REACT_APP_SERVER_URL}/signup/`;
-      axios.post(url, payload)
-      .then(response => {
-        console.log(response.data);
-        const { token } = response.data;
-        localStorage.setItem('jwtToken', token);
-        setAuthToken(token)
-        const decoded = jwt_decode(token);  // Decode token to get the user data
-        props.nowCurrentUser(decoded);      // Set current user
-        setRedirect(true);
-      }).catch(error => {
-        console.log(error);
-        alert('Either email already exist or an error occured on our end. Please try again...');
-      })
+      const url = `${REACT_APP_SERVER_URL}/signup/`;
+      axios
+        .post(url, payload)
+        .then((response) => {
+          console.log(response.data);
+          const { token } = response.data;
+          localStorage.setItem('jwtToken', token);
+          setAuthToken(token);
+          const decoded = jwt_decode(token); // Decode token to get the user data
+          props.nowCurrentUser(decoded); // Set current user
+          setRedirect(true);
+        })
+        .catch((error) => {
+          console.log(error);
+          alert('Either email already exist or an error occured on our end. Please try again...');
+        });
     } else {
       if (!password === confirmPassword) {
         alert('Passwords do not match. Please try again...');
@@ -57,42 +59,62 @@ const Signup = (props) => {
         alert('Password needs to be at least 8 characters. Please try again...');
       }
     }
-  }
+  };
 
-  if (redirect) return <Navigate to='/contacts' />
-  
+  if (redirect) return <Navigate to="/contacts" />;
+
   return (
     <Paper>
       <Typography>
-    <div className="row mt-4">
-      <div className="col-md-7 offset-md-3">
-        <div className="card card-body">
-          <h2 className="py-2">Signup</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input type="text" name="username" value={username} onChange={handleUsername} className="form-control" />
+        <div className="row mt-4">
+          <div className="col-md-7 offset-md-3">
+            <div className="card card-body">
+              <h2 className="py-2">Signup</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="username">Username</label>
+                  <input
+                    type="text"
+                    name="username"
+                    value={username}
+                    onChange={handleUsername}
+                    className="form-control"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input type="email" name="email" value={email} onChange={handleEmail} className="form-control" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={handlePassword}
+                    className="form-control"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="confirmPassword">Verify Password</label>
+                  <input
+                    type="password"
+                    name="confirmPassword"
+                    value={confirmPassword}
+                    onChange={handleConfirmPassword}
+                    className="form-control"
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary float-right">
+                  Submit
+                </button>
+              </form>
             </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input type="email" name="email" value={email} onChange={handleEmail} className="form-control" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input type="password" name="password" value={password} onChange={handlePassword} className="form-control" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="confirmPassword">Verify Password</label>
-              <input type="password" name="confirmPassword" value={confirmPassword} onChange={handleConfirmPassword} className="form-control" />
-            </div>
-            <button type="submit" className="btn btn-primary float-right">Submit</button>
-          </form>
+          </div>
         </div>
-      </div>
-    </div>
-    </Typography>
+      </Typography>
     </Paper>
-  )
-}
+  );
+};
 
 export default Signup;
