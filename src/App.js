e9074fbd5338ffc7d875c94897ca77e9b92b99fa
AntoineCharette <antoineocharette@gmail.com>
2021-12-28 23:12:34 -0500

    modified the navbar

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import About from './components/about/About';
 import Footer from './components/footer/Footer';
 import Login from './components/login/Login';
 import Contacts from './components/contacts/Contacts';
+import AddFriend from './components/addFriend/AddFriend';
 
 function App(props) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -23,7 +24,7 @@ function App(props) {
   const chooseIconMode = darkMode ? <Brightness4Icon /> : <Brightness5Icon />;
 
   const toggleLoggedIn = () => {
-    setIsLoggedIn(!isLoggedIn)
+    setIsLoggedIn(!isLoggedIn);
   };
 
   const changeTheme = createTheme({
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,8 @@
+// Imports
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+// Styling
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -9,26 +13,17 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
-import { Link } from 'react-router-dom';
 import logo from '../../img/logo.png';
-import { makeStyles } from '@mui/styles';
+import './Navbar.scss';
 
-const useStyles = makeStyles({
-  logo: {
-    height: '100px',
-  },
-});
-
-const pages = ['About', 'Add Friend'];
+const pages = ['Contacts', 'Add Friend'];
 const links = {
-  'About': '/about',
-  'Add Friend': '/addfriend'
-}
+  Contacts: '/',
+  'Add Friend': '/addfriend',
+};
 
 const NavBar = (props) => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
-
-  const classes = useStyles();
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -42,78 +37,72 @@ const NavBar = (props) => {
     <AppBar className="navbar nav-bg" position="static" color="primary">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-          >
-            <img src={logo} alt="SecuriChat" className={classes.logo} />
+          <Typography variant="h6" noWrap component="div" sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}>
+            <Link to="/">
+              <img src={logo} alt="SecuriChat" className="logo" />
+            </Link>
           </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">
-                    <Link to={links[page]} className="nav-link-dropdown">{page}</Link>
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-          >
-            <img src={logo} alt="SecuriChat" className={classes.logo} />
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <Link to="/">
+              <img src={logo} alt="SecuriChat" className="logo" />
+            </Link>
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                <Link to={links[page]} className="nav-link">{page}</Link>
-              </Button>
-            ))}
-          </Box>
-          <Button color="inherit">
-            {props.isLoggedIn ? 
-            <></> :
-            <Link to='/Login' className="nav-link">Login</Link>}
-          </Button>
-          <IconButton color="inherit" onClick={() => props.setDarkMode(!props.darkMode)}>
+          {props.isLoggedIn ? (
+            <>
+              <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: 'block', md: 'none' },
+                  }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center">
+                        <Link to={links[page]} className="nav-link-dropdown">
+                          {page}
+                        </Link>
+                      </Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                {pages.map((page) => (
+                  <Button key={page} onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>
+                    <Link to={links[page]} className="nav-link">
+                      {page}
+                    </Link>
+                  </Button>
+                ))}
+              </Box>
+            </>
+          ) : (
+            <></>
+          )}
+          <IconButton id="color-selector" color="inherit" onClick={() => props.setDarkMode(!props.darkMode)}>
             {props.chooseIconMode}
           </IconButton>
         </Toolbar>
